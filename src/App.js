6c03d8f064dbedb56b9d47cd0e8a76204224488f
harishdandu <harish.dandu@archents.com>
2024-02-12 17:05:19 +0530

    Push updated code

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/dashboard';
 import AdminUserRoutes from './Routing/AdminUserRoutes';
 import AdminDashboard from './components/Admin/Admin';
+import { LoaderProvider } from '../src/services/LoaderContext';
 
 function App() {
   return (
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/user-dashboard/*" element={<Dashboard />} />
-        <Route path="/Admin/*" element={<AdminUserRoutes />} />
-        <Route path="/admin-dashboard/*" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+    <LoaderProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/user-dashboard/*" element={<Dashboard />} />
+          <Route path="/Admin/*" element={<AdminUserRoutes />} />
+          <Route path="/admin-dashboard/*" element={<AdminDashboard />} />
+        </Routes>
+      </Router>
+    </LoaderProvider>
   );
 }
 
