dfa763d46e0e08b7c76a4c9f50b1f348ea8fd643
harishdandu <harish.dandu@archents.com>
2024-02-13 11:44:53 +0530

    toaster

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,44 +2,51 @@ import React, { useState } from 'react';
 import { login } from '../services/authService';
 import { useNavigate } from 'react-router-dom';
 import styles from '../Styles/User/UserLogin.module.css';
-import { useForm } from 'react-hook-form';
-
-
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function LoginForm() {
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = (data) => {
-        // e.preventDefault();
-        setIsLoading(true);
-        console.log(data, errors);
-        // try {
-        //     // Call the login service function
-        //     let reqobj = {
-        //         userName: email,
-        //         role: "DOCTOR",
-        //         password: password,
-        //         deviceToken: ""
-        //     }
-        //     const response = await login(reqobj);
-        //     if (response.errors === "") {
-        //         // debugger;
-        //         sessionStorage.setItem("LoginResponse", JSON.stringify(response));
-        //         sessionStorage.setItem('providerID', response.user._id);
-        //         console.log('Login successful component', response);
-        //         navigate('/user-dashboard/home');
-        //     }
-        // } catch (error) {
-        //     console.error('Login failed:', error);
-        // }
+    // const { isLoading, showLoader, hideLoader } = useLoader();
+    const [isLoading, setIsLoading] = useState(false);
 
-        setIsLoading(false);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            // Call the login service function
+            let reqobj = {
+                userName: email,
+                role: "DOCTOR",
+                password: password,
+                deviceToken: ""
+            };
+            setIsLoading(true);
+            const response = await login(reqobj);
+            if (response.errors === "") {
+                // debugger;
+                setIsLoading(false);
+                sessionStorage.setItem("LoginResponse", JSON.stringify(response));
+                sessionStorage.setItem('providerID', response.user._id);
+                console.log('Login successful component', response);
+                navigate('/user-dashboard/home');
+            } else {
+                // debugger;
+                toast.success('This is a failuer toast!');
+                setIsLoading(false);
+            }
+        } catch (error) {
+            setIsLoading(false);
+            console.error('Login failed:', error);
+        }
+
     };
 
     return (
+
         <div id="12218" className="container-fluid">
             <div id="12219" className="row">
                 <div id="12220" className="col-lg-5 col-md-6 col-sm-5  col-12 px-0">
@@ -56,25 +63,22 @@ function LoginForm() {
 
                         </div>
                         <div className={styles.userloginloginform}>
-                            <form onSubmit={handleSubmit(onSubmit)} className='row'>
+                            <form onSubmit={handleSubmit} className='row'>
                                 <div className={`row ${styles.eachrow}`}>
                                     {/* <label htmlFor="email">Email:</label> */}
                                     <div className={styles.materialtextfield}>
                                         <input
                                             type="email"
                                             className={`form-control ${styles.matInput}`}
-                                            id="email" placeholder=""
-                                            {...register('email', {
-                                                required: 'Email is required',
-                                                pattern: {
-                                                    value: /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w[a-z]{1,2})+$/,
-                                                    message: 'Please enter a valid email address'
-                                                }
-                                            })}
+                                            id="email" placeholder="Email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            required
+
                                         />
                                         <label htmlFor="email" className={styles.matLabel}>Email</label>
-                                        {errors.email && <div className="error">{errors.email.message}</div>}
                                     </div>
+
                                 </div>
                                 <div className='row each-row'>
                                     <div className={styles.materialtextfield}>
@@ -83,14 +87,12 @@ function LoginForm() {
                                             type="password"
                                             id="password"
                                             className={`form-control ${styles.matInput}`}
-                                            placeholder=""
-                                            {...register('password', {
-                                                required: 'password is required',
-
-                                            })}
+                                            placeholder="Password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            required
                                         />
                                         <label htmlFor="password" className={styles.matLabel}>Password</label>
-                                        {errors.password && <div className="error">{errors.password.message}</div>}
                                     </div>
                                 </div>
                                 <div className={styles.eachrowbtn}>
@@ -115,6 +117,15 @@ function LoginForm() {
                     </div>
                 </div>
             </div>
+
+            {isLoading && (
+                <div className='loader-container'>
+                    <div className="loader-overlay">
+                        <img src='../images/provider_isometric.png' alt="Loading..." className="loader-img" />
+                    </div>
+                </div>
+            )}
+            <ToastContainer />
         </div>
     );
 }
